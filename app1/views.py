761c2ed7fffc2be2007120085e3e21a336eddbfa
nic9las17 <nicolas.ahumada.ojeda@gmail.com>
2026-08-24 14:16:35 -0400

    guardado

--- app1/views.py
+++ app1/views.py
@@ -5,7 +5,6 @@ def v1_app1(request):
     return HttpResponse ("<h1>vista 1 app1</h1>")
 
 def v2_app2(request):
-    return HttpResponse ("<h1>vista 2 app1>/h1>"
+    return HttpResponse ("<h1>vista 2 app1</h1>"
     "<p>todo lo que necesitas</p>")
                         
-
